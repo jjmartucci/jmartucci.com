94a2c182a341405df1a62544ec38fed6202579d7
Joe Martucci <joe.martucci@gmail.com>
2025-05-22 19:18:58 -0400

    Test Netlify adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import netlify from '@astrojs/netlify';
 
 import sitemap from "@astrojs/sitemap";
 import {gardenFrontMatterPlugin} from "./remark-plugins/garden-frontmatter.mjs";
@@ -10,12 +11,16 @@ import {externalLink} from "./remark-plugins/link-rewrite.ts";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jmartucci.com",
+  adapter: netlify(),
+
   integrations: [mdx({
     remarkPlugins: [gardenFrontMatterPlugin],
   }), sitemap()],
+
   markdown: {
     rehypePlugins: [externalLink],
     remarkPlugins: [gardenFrontMatterPlugin]
   },
 
+  output: "server",
 });
